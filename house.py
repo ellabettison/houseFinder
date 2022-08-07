421e239dbc3c08758dd360f5b5960b79137c3b5c
Ella <efb4518@ic.ac.uk>
2022-08-07 19:46:26 +0100

    Files

---
+++ house.py
@@ -0,0 +1,185 @@
+from selenium import webdriver
+from time import sleep
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+chromdrive_path = '/usr/bin/chromedriver'
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(chromdrive_path, options=chrome_options)
+
+supermarkets_to_check = ['Tesco', 'Sainsbury', 'Morrisons', 'ASDA', 'ALDI', 'M&S', 'Waitrose', 'Co-op', 'Iceland',
+                         'Lidl']
+
+
+class House:
+    def __init__(self, url, address, price):
+        self.url = url
+        self.address = address
+        self.price = price
+        self.ella_time = None
+        self.matt_time = None
+        self.closest_station = None
+        self.closest_station_dist = None
+        self.closest_shop = None
+        self.closest_shop_dist = None
+        self.crime_rate = None
+
+    def __repr__(self):
+        return f"address: {self.address}, \t price: {self.price}, \t Ella time: {self.ella_time}, \t Matt time: {self.matt_time}, \t url: {self.url}"
+
+    def get_travel_dist(self, start, ella_dist=True):
+        url = f"https://www.google.co.uk/maps/search/{self.address.replace(' ', '+')}+to+{start.replace(' ', '+')}"
+
+        driver.get(url)
+
+        try:
+            reject_button = driver.find_element_by_css_selector("button[aria-label='Reject all']")
+            reject_button.click()
+        except:
+            pass
+
+        sleep(1)
+
+        try:
+            public_transport_div = driver.find_element_by_css_selector("div[data-travel_mode='3']")
+            public_transport_button = public_transport_div.find_element_by_css_selector("button")
+
+            public_transport_button.click()
+
+            sleep(0.5)
+        except:
+            return 1111111
+
+        try:
+            depart_at_div = driver.find_element_by_class_name("goog-menu-button-outer-box")
+            depart_at_div.click()
+
+            sleep(0.2)
+
+            menu = driver.find_element_by_class_name("goog-menu-BvBYQ")
+            depart_at_option = menu.find_element_by_id(":1")
+            depart_at_option.click()
+
+            sleep(0.2)
+
+            time_input = driver.find_element_by_class_name("LgGJQc")
+            time_input.send_keys(Keys.CONTROL + "a")
+            time_input.send_keys(Keys.DELETE)
+            time_input.send_keys("16:00")
+
+            sleep(2)
+
+        except:
+            return 2222222
+
+        try:
+            distance_section = driver.find_element_by_id('section-directions-trip-0')
+            distance = distance_section.find_element_by_xpath("//div[contains(text(),' min')]")
+        except:
+            return 333333333
+
+        distance = distance.get_attribute('innerHTML')
+
+        mins = distance.split(' min')[0]
+        if "hr" in mins:
+            mins = mins.split(' hr ')
+            mins = int(mins[0]) * 60 + int(mins[1])
+
+        if ella_dist:
+            self.ella_time = int(mins)
+        else:
+            self.matt_time = int(mins)
+
+        return int(mins)
+
+    def check_for_shared_housing(self):
+        pass
+
+    def find_area_info(self):
+        driver.get(f"https://www.streetcheck.co.uk/search?s={self.address.replace(' ', '+')}")
+        sleep(2)
+        search_results = driver.find_element_by_css_selector("ul[id='searchresults']")
+        first_result = search_results.find_element_by_css_selector('li').find_element_by_css_selector('a')
+
+        postcode = first_result.get_attribute('href').replace('https://www.streetcheck.co.uk/postcode/', '')
+
+        driver.get(f"https://www.postcodearea.co.uk/postaltowns/london/{postcode}/")
+        stats = driver.find_elements_by_id("marital-status")
+
+        ethnicities = stats[1]
+        unemployment = stats[2]
+
+        ethnicities_info = ethnicities.find_element_by_css_selector("p").text
+        unemployment_info = unemployment.find_element_by_css_selector("p").text
+
+        print(ethnicities_info)
+        print(unemployment_info)
+
+        # crime rate
+
+        driver.get(f"https://www.postcodearea.co.uk/postaltowns/london/{postcode}/crime/")
+        crimes_table = driver.find_element_by_xpath("//table[contains(@class,'table-crime')]")
+        crimes_rows = crimes_table.find_elements_by_css_selector("tr")
+
+        crimes_info = []
+
+        total_local_crimes = 0
+        total_uk_crimes = 0
+
+        for row in crimes_rows[1:]:
+            cols = row.find_elements_by_css_selector("td")
+            row_name = cols[0].text
+            uk_avg = int(cols[2].text)
+            this_area = int(cols[3].text)
+
+            if this_area > uk_avg * 1.5:
+                crimes_info.append(
+                    f"{row_name} rates are {(int((max(this_area, 1) / max(1, uk_avg)) * 100)) / 100} times the UK average")
+            if this_area < uk_avg / 1.5:
+                crimes_info.append(
+                    f"{row_name} rates are {(int((max(1, uk_avg) / max(1, this_area)) * 100)) / 100} times lower than "
+                    f"the UK average")
+
+            total_local_crimes += this_area
+            total_uk_crimes += uk_avg
+
+        print(crimes_info)
+        self.crime_rate = total_local_crimes / total_uk_crimes
+        print(f"crime rate compared to uk average: {self.crime_rate}")
+
+        # closeness to stations
+
+        driver.get(f"https://postcodebyaddress.co.uk/{postcode}")
+        train_stations_table = driver.find_element_by_id("railway-stations-table")
+        closest_station = train_stations_table.find_elements_by_css_selector("tr")[1]
+        closest_station_cols = closest_station.find_elements_by_css_selector("td")
+        self.closest_station = closest_station_cols[0].text
+        self.closest_station_dist = float(closest_station_cols[1].text[:-2])
+
+        # closest shop
+
+        supermarkets_table = driver.find_element_by_id("supermarkts-table")
+
+        selector = "//td["
+        for shop in supermarkets_to_check:
+            selector += f"contains(text(),'{shop}')"
+            selector += " or "
+        selector = selector[:-3]
+        selector += "]"
+
+        closest_supermarket = supermarkets_table.find_elements_by_xpath(selector)[0].find_element_by_xpath('..')
+        closest_supermarket_cols = closest_supermarket.find_elements_by_css_selector("td")
+        self.closest_shop = closest_supermarket_cols[0].text
+        self.closest_shop_dist = float(closest_supermarket_cols[1].text[:-2])
+
+        print(self.closest_station, self.closest_station_dist)
+        print(self.closest_shop, self.closest_shop_dist)
+
+        print("\n ============= \n")
+
+
+if __name__ == '__main__':
+    house = House('www.google.com', 'al35rz', 100)
+    house.get_travel_dist('aalborg')
---
+++ main.py
@@ -0,0 +1,172 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from house import House
+from time import sleep
+import pandas as pd
+
+chromedrive_path = '/usr/bin/chromedriver'
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(chromedrive_path, options=chrome_options)
+
+ella_work_loc = 'kings+cross'
+ella_max_travel_time_mins = 35
+
+matt_work_loc = 'imperial+college+london'
+matt_max_travel_time_mins = 60
+
+max_crime_rate = 1.5
+max_shop_distance = 2
+max_any_shop_distance = 0.5
+max_train_distance = 3
+
+
+class HouseFinder:
+    def __init__(self, max_price):
+        self.max_price = max_price
+        self.rightmove_url = f"https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=REGION^87523&maxBedrooms=1&minBedrooms=1&maxPrice={self.max_price}&radius=5.0&sortType=1&propertyTypes=&mustHave=&dontShow=houseShare%2Cretirement%2Cstudent&furnishTypes=&keywords="
+
+    def find_houses_rightmove(self):
+        driver.get(self.rightmove_url)
+
+        houses = []
+
+        for _ in range(20):
+            houses_list_container = driver.find_element_by_id("l-searchResults")
+            houses_list = houses_list_container.find_elements_by_class_name("l-searchResult")
+
+            for house in houses_list:
+                url = house.find_element_by_css_selector('a[data-test="property-details"]')
+                address = house.find_element_by_class_name('propertyCard-address')
+                price = house.find_element_by_class_name('propertyCard-priceValue')
+
+                new_house = House(url=url.get_attribute("href"), address=address.text,
+                                  price=price.text)
+
+                houses.append(new_house)
+
+            next_button = driver.find_element_by_css_selector("button[data-test='pagination-next']")
+
+            if next_button.is_enabled():
+                next_button.click()
+                sleep(2)
+            else:
+                break
+
+        return houses
+
+    def find_houses_zoopla(self):
+        driver.get(
+            f"https://www.zoopla.co.uk/to-rent/property/mayfair?beds_max=1&beds_min=1&page_size=25&price_frequency=per_month&price_max={self.max_price}&view_type=list&q=Mayfair%2C+London&radius=5&results_sort=lowest_price&search_source=refine&include_shared_accommodation=false&keywords=-studio")
+        houses = []
+
+        sleep(1)
+
+        try:
+            cookies_button = driver.find_element_by_css_selector("button[id='save']")
+            cookies_button.click()
+        except:
+            pass
+
+        for _ in range(20):
+            try:
+                houses_list_container = driver.find_element_by_css_selector("main[data-testid='search-content']")
+                houses_list = houses_list_container.find_elements_by_css_selector("div[data-testid='search-result']")
+            except:
+                cookies_button = driver.find_element_by_css_selector("button[id='save']")
+                cookies_button.click()
+                houses_list_container = driver.find_element_by_css_selector("main[data-testid='search-content']")
+                houses_list = houses_list_container.find_elements_by_css_selector("div[data-testid='search-result']")
+
+            for house in houses_list:
+                url = house.find_element_by_css_selector('a[data-testid="listing-details-link"]')
+                address = house.find_element_by_css_selector("p[data-testid='listing-description']")
+                price = house.find_element_by_css_selector(
+                    "div[data-testid='listing-price']").find_element_by_css_selector("p")
+
+                new_house = House(url=url.get_attribute("href"), address=address.text,
+                                  price=price.text)
+
+                houses.append(new_house)
+
+            try:
+                next_button = driver.find_element_by_xpath("//a[contains(@class, 'PaginationItemNext')]")
+            except:
+                return houses
+
+            if next_button.get_attribute("aria_disabled") == "false":
+                next_button.click()
+                sleep(3)
+            else:
+                return houses
+
+            try:
+                alert_button = driver.find_element_by_css_selector("button[data-testid='modal-close']")
+                alert_button.click()
+            except:
+                pass
+
+        return houses
+
+    def find_house_distance(self, house):
+        ella_distance = house.get_travel_dist(ella_work_loc)
+        if ella_distance > ella_max_travel_time_mins:
+            return ella_distance, 9999999
+        matt_distance = house.get_travel_dist(matt_work_loc, ella_dist=False)
+
+        return ella_distance, matt_distance
+
+    def find_suitable_houses(self):
+        houses = self.find_houses_rightmove()
+        houses += self.find_houses_zoopla()
+
+        suitable_houses = []
+
+        for i, house in enumerate(houses):
+            ella_distance, matt_distance = self.find_house_distance(house)
+            if ella_distance <= ella_max_travel_time_mins and matt_distance <= matt_max_travel_time_mins:
+                suitable_houses.append(house)
+                print(f"House {i}/{len(houses)} is SUITABLE    ......    {ella_distance} mins | {matt_distance} mins  "
+                      f"| URL: {house.url}")
+            else:
+                print(f"House {i}/{len(houses)} is UNSUITABLE  ......    {ella_distance} mins | {matt_distance} mins  "
+                      f"| URL: {house.url}")
+
+        final_suitable_houses = []
+
+        for house in suitable_houses:
+            house.find_area_info()
+            if house.crime_rate < max_crime_rate and house.closest_shop_dist < max_shop_distance and house.closest_station_dist < max_train_distance:
+                final_suitable_houses.append(house)
+
+        dataframe = pd.DataFrame(
+            columns=['Address', 'Price', 'Ella Time', 'Matt Time', 'Crime Rate', 'Closest Shop', 'Shop Distance',
+                     'Closest Station', 'Station Distance', 'URL'])
+
+        for house in final_suitable_houses:
+            dataframe = dataframe.append({"Address": house.address,
+                                          "Price": house.price,
+                                          "Ella Time": house.ella_time,
+                                          "Matt Time": house.matt_time,
+                                          "Crime Rate": house.crime_rate,
+                                          "Closest Shop": house.closest_shop,
+                                          "Shop Distance": house.closest_shop_dist,
+                                          "Closest Station": house.closest_station,
+                                          "Station Distance": house.closest_station_dist,
+                                          "URL": house.url}, ignore_index=True)
+
+        print(final_suitable_houses)
+        with pd.option_context('display.max_rows', None, 'display.max_columns',
+                               None):  # more options can be specified also
+            print(dataframe)
+
+        dataframe.to_csv("houses.csv")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    house_finder = HouseFinder(max_price=1250)
+    house_finder.find_suitable_houses()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
